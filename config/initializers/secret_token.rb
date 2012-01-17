a2182cbf28d4e91a421ec8063c590c01badcad9f
Yoshihide Chubach (Luna CentOS) <yoshi@chubachi.net>
2012-01-18 03:03:24 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AiitFbAdmissions::Application.config.secret_token = '1b48b98d3387bf7bb39349acdae3c1a4dc32c1c57c7cf30e4a648652b1507c9ec140d7ad9ecde685617d7fd3f26f8c5f79b1b7ef195b0505c619219a497e793d'
