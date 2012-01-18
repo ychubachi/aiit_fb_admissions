58fd0507fd305c802659e6084aef17ebfed9a4c1
Yoshihide Chubach (Luna CentOS) <yoshi@chubachi.net>
2012-01-18 15:38:14 +0900

    Commited

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -9,9 +9,15 @@ class IndexController < ApplicationController
   # GET /index.json
   def show
     puts '=' * 8 + ' GET /index'
+    respond_to do |format|
+      format.html {render template: 'index/show', layout: 'pagetab'}
+    end
   end
 
   def create
     puts '=' * 8 + ' POST /index'
+    respond_to do |format|
+      format.html {render template: 'index/show', layout: 'canvas'}
+    end
   end
 end
